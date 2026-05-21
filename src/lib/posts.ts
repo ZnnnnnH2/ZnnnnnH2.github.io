d7684d737195ce3d8723e76dac69324829f673fa
ZnnnnnH2 <2024201540@ruc.edu.cn>
2026-05-21 15:57:05 +0800

    Add post utility helpers

---
+++ src/lib/posts.ts
@@ -0,0 +1,22 @@
+import { getCollection } from 'astro:content';
+
+export async function getAllPosts() {
+  const posts = await getCollection('blog', ({ data }) => !data.draft);
+  return posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
+}
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('zh-CN', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(date);
+}
+
+export function uniqueTags(posts: Awaited<ReturnType<typeof getAllPosts>>) {
+  return [...new Set(posts.flatMap(post => post.data.tags ?? []))].sort((a, b) => a.localeCompare(b));
+}
+
+export function uniqueCategories(posts: Awaited<ReturnType<typeof getAllPosts>>) {
+  return [...new Set(posts.map(post => post.data.category).filter(Boolean) as string[])].sort((a, b) => a.localeCompare(b));
+}
